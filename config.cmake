105c44b80bfe53f54935d61935453595728f3a59
yangzelin <yhuan416@foxmail.com>
2024-02-20 15:03:06 +0800

    update add hash likely tally

--- config.cmake
+++ config.cmake
@@ -1,4 +1,5 @@
 
 set(WARN_CFLAGS -Wall -Wstrict-prototypes -Wold-style-definition -Wundef -Wmissing-prototypes -Wmissing-declarations -Wpointer-arith -Wwrite-strings -Wshadow=local)
 
-set(CCAN_CFLAGS -g3 -ggdb ${WARN_CFLAGS} -DCCAN_STR_DEBUG=1)
+# set(CCAN_CFLAGS -g3 -ggdb ${WARN_CFLAGS} -DCCAN_STR_DEBUG=1)
+set(CCAN_CFLAGS -O3 ${WARN_CFLAGS} -DCCAN_STR_DEBUG=1)
